bf5a02981a7d621bdded87d95aa70e85b3d7f28b
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 06:28:57 -0500

    Detect last created token

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js — CAMBIA SOLO ESTO
 module.exports = {
-  DEV_WALLET: 'Gv4E5a4GYvuHjGNUkdEFtjDdEzzxGUC49w4TWQwUAenu',     // ← Pega tu wallet dev
+  DEV_WALLET: '4mPJC1ZomcYFKMsQSmLW31Xz1rFvpPFqKxTK4HiA1KFL',     // ← Pega tu wallet dev
   HELIUS_API_KEY: '95932bca-32bc-465f-912c-b42f7dd31736', // ← Tu key
   PUMP_API_KEY: '9tqmyub3anw66v35dcqm4jjfatpq2c388x84jkak6n2pythr6hkk0gv960rqcphr9dc76xaf6x66ehvr74w66xub5wu6jk3rcdx5eya8an4q4yka994puwbn913pakjm71c4gt3gcwykudn7n0hkhb9j6gvjtedkngyjfawf1cmrv2cf1k4cmj36x2p2n2tdnwp6xka5x8kuf8',
   SOL_TO_SPEND: 0.5,
