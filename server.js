3dddf2b5411289aee21321204e207882846fe9e6
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 05:28:39 -0500

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 // === TUS DATOS (MAINNET) ===
 const PUMP_API_KEY = '9x7pwjube5wq0tagf5r34nb169mkjc9bdxj34v9m6n97ghk1a90n6x3ma58ppu37ct4q6j3qdthm2kbgb9gq8gk3cnb6pnhr6wt7mp33a59njt3p8d7k0e3nb1w4jrvm88nq8pauewyku9x77jka6adcpge1g8x2m2kad84e9nk6m3tegun6gtpcgt5crjmd4npajumcnvkuf8';
-const TOKEN_MINT = '3uVFtitJozUMrsxakiqwitX1N3f2Vrc4KQT49Hucpump';
+const TOKEN_MINT = '8zvY5yQH5NDB61heawTNHuk7WyFRZPeoFvMCL6Lbpump';
 const TOKENS_REQUIRED = 1;
 const SOL_TO_SPEND = 0.001;
 
