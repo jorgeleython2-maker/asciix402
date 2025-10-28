3eb6f3aa3d99338ac5838cdfa599632cd5fa9f5b
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 07:25:14 -0500

    Token detection working

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 module.exports = {
   TOKEN_MINT: 'CYtHzg3shYXcaLjhq6vHRPv31Bf91pGm7wsNScYTpump', // ← TU MINT AQUÍ
   PUMP_API_KEY: '9x7pwjube5wq0tagf5r34nb169mkjc9bdxj34v9m6n97ghk1a90n6x3ma58ppu37ct4q6j3qdthm2kbgb9gq8gk3cnb6pnhr6wt7mp33a59njt3p8d7k0e3nb1w4jrvm88nq8pauewyku9x77jka6adcpge1g8x2m2kad84e9nk6m3tegun6gtpcgt5crjmd4npajumcnvkuf8',
-  SOL_TO_SPEND: 0.5,
+  SOL_TO_SPEND: 0.0,
   X_HANDLE: 'TU_USUARIO_X'
 };
