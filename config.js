f5167861abeab1961fc92da9840544aef1579612
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 06:10:47 -0500

    Fix: hardcoded mint for reliability

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js — CAMBIA SOLO ESTO
 module.exports = {
-  DEV_WALLET: 'TU_WALLET_DEV_AQUI',     // ← Tu wallet dev
+  DEV_WALLET: '3HKkfFWY8uoQWff5J38AGCRUTbameCkLGUWt3F88K4r4',     // ← Tu wallet dev
   TOKEN_MINT: '3EiUGPmePL6bNSyE6TphBRYsLoFqeNdrPUBwc5R4pump', // ← Hardcodeado temporal (cambia por el de tu wallet)
   PUMP_API_KEY: '9x7pwjube5wq0tagf5r34nb169mkjc9bdxj34v9m6n97ghk1a90n6x3ma58ppu37ct4q6j3qdthm2kbgb9gq8gk3cnb6pnhr6wt7mp33a59njt3p8d7k0e3nb1w4jrvm88nq8pauewyku9x77jka6adcpge1g8x2m2kad84e9nk6m3tegun6gtpcgt5crjmd4npajumcnvkuf8', // ← Tu API key
   SOL_TO_SPEND: 0.5,
