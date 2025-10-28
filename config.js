f6f05c64416d6027ce67ab2bede62670d4a84540
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 07:07:11 -0500

    Token detection working

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js — CAMBIA SOLO LA WALLET DEV
 module.exports = {
-  DEV_WALLET: '2AGiSPpLTwTzN1jLL4ZetfS5nYbiSSfempThxdBHpump', // ← Tu wallet dev
+  DEV_WALLET: '4mPJC1ZomcYFKMsQSmLW31Xz1rFvpPFqKxTK4HiA1KFL', // ← Tu wallet dev
   BITQUERY_API_KEY: 'PK68uHGc-DuVg0G9ScfRyWZVCmPUzbeA_0Ois1orWGQ.9LFTaMaXF6h5osioNEsfkgfNb-Egu4s3YWPDGG3jhKY',
   PUMP_API_KEY: '9x7pwjube5wq0tagf5r34nb169mkjc9bdxj34v9m6n97ghk1a90n6x3ma58ppu37ct4q6j3qdthm2kbgb9gq8gk3cnb6pnhr6wt7mp33a59njt3p8d7k0e3nb1w4jrvm88nq8pauewyku9x77jka6adcpge1g8x2m2kad84e9nk6m3tegun6gtpcgt5crjmd4npajumcnvkuf8',
   SOL_TO_SPEND: 0.5,
