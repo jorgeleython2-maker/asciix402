57df3c46c9ddc7d6c1501a61d4575e6f368aa0f8
jorgeleython2-maker <jorgeleython2@gmail.com>
2025-10-28 06:22:22 -0500

    Detect last traded token

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js — CAMBIA SOLO LA WALLET Y KEYS
 module.exports = {
-  DEV_WALLET: '5RycxTg9L97iGePnu42B4Q3mbu87E2mjqSYHMXywuSH4', // ← Tu wallet dev (cambia aquí)
+  DEV_WALLET: 'Gv4E5a4GYvuHjGNUkdEFtjDdEzzxGUC49w4TWQwUAenu', // ← Tu wallet dev (cambia aquí)
   HELIUS_API_KEY: '95932bca-32bc-465f-912c-b42f7dd31736', // ← Tu key
-  PUMP_API_KEY: '9x7pwjube5wq0tagf5r34nb169mkjc9bdxj34v9m6n97ghk1a90n6x3ma58ppu37ct4q6j3qdthm2kbgb9gq8gk3cnb6pnhr6wt7mp33a59njt3p8d7k0e3nb1w4jrvm88nq8pauewyku9x77jka6adcpge1g8x2m2kad84e9nk6m3tegun6gtpcgt5crjmd4npajumcnvkuf8', // ← PumpPortal
+  PUMP_API_KEY: '9tqmyub3anw66v35dcqm4jjfatpq2c388x84jkak6n2pythr6hkk0gv960rqcphr9dc76xaf6x66ehvr74w66xub5wu6jk3rcdx5eya8an4q4yka994puwbn913pakjm71c4gt3gcwykudn7n0hkhb9j6gvjtedkngyjfawf1cmrv2cf1k4cmj36x2p2n2tdnwp6xka5x8kuf8', // ← PumpPortal
   SOL_TO_SPEND: 0.5,
   X_HANDLE: 'TU_USUARIO_X' // ← Opcional
 };
